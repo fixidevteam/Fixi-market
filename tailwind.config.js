c23570d37a594d84b42bb5722afab878d11e6a5c
shadow007 <nourdinehoumame0@gmail.com>
2025-05-21 12:21:15 +0000

    add font to match FIXI MA

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ export default {
         secondary: "#54595f",
         accent: "#1d5197",
       },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, theme }) {
+      addBase({
+        "h1, h2, h3, h4, h5, h6": {
+          fontFamily: theme("fontFamily.montserrat"),
+        },
+        "body, p, span, a, button, input, li": {
+          fontFamily: theme("fontFamily.roboto"),
+        },
+      });
+    },
+  ],
 };
