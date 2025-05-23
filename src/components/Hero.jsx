b955e58f985db05b08fb334e65b6a5e8fb97c494
shadow007 <nourdinehoumame0@gmail.com>
2025-05-23 10:13:39 +0000

    change the font size of inputs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,14 +28,14 @@ const Hero = () => {
                   <div className="w-full md:w-1/4">
                     <input
                       type="text"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"
                       placeholder="Nom du garage"
                     />
                   </div>
 
                   {/* domaine */}
                   <div className="w-full md:w-1/4">
-                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5">
+                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5">
                       <option value="" disabled selected>
                         Tout les domaines
                       </option>
@@ -46,7 +46,7 @@ const Hero = () => {
 
                   {/* villes */}
                   <div className="w-full md:w-1/4">
-                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5">
+                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5">
                       <option value="" disabled selected>
                         Tout les villes
                       </option>
